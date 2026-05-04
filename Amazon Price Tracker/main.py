d3062dacd0ba55b650bb491f48e282fc06535e25
Morgan Toronyi <morgant@hsppr.org>
2026-05-04 09:36:50 -0600

    email send

--- Amazon Price Tracker/main.py
+++ Amazon Price Tracker/main.py
@@ -6,22 +6,28 @@ import smtplib
 
 load_dotenv()
 
-url = 'https://appbrewery.github.io/instant_pot/'
-response = requests.get(url)
+load_dotenv()
+print(repr(os.environ.get('EMAIL_ADDRESS')))
+print(repr(os.environ.get('EMAIL_PASSWORD')))
+
+url = 'https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6'
+response = requests.get(url, 
+    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 Edg/147.0.0.0",
+    "Accept-Language": "en-US,en;q=0.9"})
 
 soup = BeautifulSoup(response.text, 'html.parser')
-price = soup.find(name='span', class_='aok-offscreen').getText()
-price_text = price.split('$')[1]
-price_float = float(price_text)
-print(price_float)
+price_whole = soup.find(name='span', class_='a-price-whole').getText()
+price_fraction = soup.find(name='span', class_='a-price-fraction').getText()
+price = price_whole + price_fraction
+price = float(price)
 
 
 title = soup.find(id='productTitle').getText().strip()
 print(title)
 
-BUY_PRICE = 100
+BUY_PRICE = 200
 
-if price_float < BUY_PRICE:
+if price < BUY_PRICE:
     message = f'{title} is on sale for {price}!'
 
     with smtplib.SMTP(os.environ['SMTP_ADDRESS'], port=587) as connection:
